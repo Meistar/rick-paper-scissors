5d67eef1a01bf89cde790c6b0f9218323665dd6b
Egor Kolesnik <egorisk1998@gmail.com>
2025-04-20 20:34:02 +0300

    changed the computerChoice function and started to work on the gameRound function

--- scripts.js
+++ scripts.js
@@ -5,19 +5,51 @@ function getComputerChoice() {
     let ranNum = Math.floor(Math.random() * (4 - 1) + 1);
 
     if (ranNum === 1) {
-        return console.log("Rock");
+        ranNum = "rock";
     } else if (ranNum === 2) {
-        return console.log("paper");
+        ranNum = "paper";
     } else if (ranNum === 3) {
-        return console.log("scissors");
+        ranNum = "scissors";
     }
+    return console.log(ranNum);
 }
 
-getComputerChoice();
+// create a function named 'getHumanChoice' that asks the user for input (rock,paper or scissors).
+//right now it doesnt have a way to check if the input is valid
+
+const computerSelection = getComputerChoice();
 
 function getHumanChoice() {
-    let input = prompt("whats your pick?");
-    console.log(input);
+    let input = prompt("whats your pick?").toLowerCase();
+
+    return console.log(input);
 }
 
-getHumanChoice();
+const humanSelection = getHumanChoice();
+
+//create a way to keep track of the score of the player(human and computer)
+
+let humanScore = 0;
+let computerScore = 0;
+
+//a function that starts the game, one round
+//IF human wins 'You win! {humanChoice} beats {computerChoice}
+//IF computer wins 'You lose! {computerChoice} beats {humanChoice}
+
+function playRound(humanSelection, computerSelection) {
+    if (
+        (humanSelection === "rock" && computerSelection === "scissors") ||
+        (humanSelection === "paper" && computerSelection === "rock") ||
+        (humanSelection === "scissors" && computerSelection === "paper")
+    ) {
+        humanScore++;
+        return console.log(`You win! ${humanSelection} beats ${computerSelection}`);
+    } else if (humanSelection === humanSelection) {
+        return console.log("Its a tie!");
+    } else {
+        return console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    }
+}
+console.log(computerSelection);
+console.log(humanSelection);
+const roundResult = playRound(humanSelection, computerSelection);
